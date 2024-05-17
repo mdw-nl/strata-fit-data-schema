abcf0afb532f3d21343ca926aad64f8d636f0275
ivanzhovannik <ivan.zhovannik@medicaldataworks.nl>
2024-05-17 15:41:07 +0200

    better UX

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import JSONResponse
-from typing import List, Optional
+from fastapi import FastAPI, Response, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse
 import pandas as pd
 import io
+import os
 
 from config.config import settings
 from schema import ValidationReport
@@ -13,11 +13,21 @@ app = FastAPI(
     title=settings.openapi.title,
     description=settings.openapi.description,
     version=settings.openapi.version,
-    lifespan=None
+    lifespan=None,
+    contact=settings.openapi.contact
 )
 
 
-@app.post("/validate", response_model=ValidationReport)
+@app.get("/settings", tags=["Validation"])
+def get_settings():
+    settings_file_path = settings.openapi.settings_path
+    if os.path.exists():
+        return FileResponse(settings_file_path)
+    else:
+        return Response(content="Settings file not found.", status_code=404)
+
+
+@app.post("/validate", response_model=ValidationReport, tags=["Validation"])
 async def validate_csv_file(file: UploadFile = File(...)):
     if not file.filename.endswith('.csv'):
         raise HTTPException(status_code=400, detail="Invalid file format. Please upload a CSV file.")
